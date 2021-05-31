e7cf722b49417707cdca63e2b07857f313ebf16c
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-31 11:26:42 -0500

    Added image from gallery option to add screen

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, Button, Image } from 'react-native';
-import { Camera } from 'expo-camera';
+import { Camera } from 'expo-camera'
+import * as ImagePicker from 'expo-image-picker';
+
 function Add() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
     const [camera, setCamera] = useState(null);
     const [image, setImage] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const galleryStatus = await Camera.requestPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
         })();
     }, []);
 
@@ -21,10 +27,24 @@ function Add() {
         }
     }
 
-    if (hasPermission === null) {
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    if (hasCameraPermission === null || hasGalleryPermission === null) {
         return <View />;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
         return <Text>No access to camera</Text>
     }
     return (
@@ -48,6 +68,7 @@ function Add() {
                 }}>
             </Button>
             <Button title="Take Photo" onPress={() => takePhoto()}/>
+            <Button title="Pick an image from gallery" onPress={() => pickImage()}/>
             {image && <Image source={{uri: image}} style={{flex: 1}} />}
         </View>
     );

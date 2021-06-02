2aaad6e036482e33e5f2f3fe9dd10eead330217e
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-06-01 21:14:10 -0500

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,127 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import jwtDecode from 'jwt-decode';
-import { logoutUser, getUserData } from './redux/actions/index';
-import axios from 'axios';
-import { 
-  API_KEY, 
-  AUTH_DOMAIN, 
-  PROJECT_ID, 
-  STORAGE_BUCKET, 
-  MESSAGING_SENDER_ID, 
-  APP_ID,
-  MEASUREMENT_ID
- } from '@env';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import firebase from 'firebase';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './redux/reducers';
-import thunk from 'redux-thunk';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import LandingScreen from './components/auth/Landing';
-import RegisterScreen from './components/auth/Register';
-import LoginScreen from './components/auth/Login';
-import MainScreen from './components/Main';
-import AddScreen from './components/main/Add';
-import SaveScreen from './components/main/Save';
-import CommentScreen from './components/main/Comment';
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-const Stack = createStackNavigator();
-
-const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID
-};
-
-if(firebase.apps.length === 0){
-  firebase.initializeApp(firebaseConfig);
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-axios.defaults.baseURL = 
-  'https://us-central1-socialape-14249.cloudfunctions.net/api'
-
-  const token = localStorage.FBIdToken;
-  if(token) {
-    const decodedToken = jwtDecode(token);
-    if(decodedToken.exp * 1000 < Date.now()) {
-      store.dispatch(logoutUser())
-      this.props.navigation.navigate("Landing")
-    } else {
-      store.dispatch({ type: SET_AUTHENTICATED });
-      axios.defaults.headers.common['Authorization'] = token;
-      store.dispatch(getUserData());
-    }
-  }
-
-export class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      loaded: false,
-    }
-  }
-
-  componentDidMount(){
-    firebase.auth().onAuthStateChanged((user) => {
-      if(!user) {
-        this.setState({
-          loggedIn: false,
-          loaded: true,
-        })
-      } else {
-        this.setState({
-          loggedIn: true,
-          loaded: true,
-        })
-      }
-    })
-  }
-
-  render() {
-    const { loggedIn, loaded } = this.state;
-    if(!loaded) {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <Text>Loading...</Text>
-        </View>
-      )
-    }
-
-    if(!loggedIn) {
-      return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Landing">
-            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
-    }
-
-    return (
-      <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Main">
-            <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
-            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
-            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
-            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
-    )
-  }
-}
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

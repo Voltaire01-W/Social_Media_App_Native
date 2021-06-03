c5c5221632ab32092eefbbb5e5cd625022c43b12
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-06-03 11:05:02 -0500

    Added config for firebase to gitignore and initialize firebase app

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import * as firebase from 'firebase';
+import firebaseConfig from './config/config';
 
 import LoadingScreen from './screens/LoadingScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator();

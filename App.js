45f7bcc30c2f08ae24f1220abc87646e848cf3af
Voltaire01-W <jamesisanerd56@gmail.com>
2021-06-03 14:04:10 -0500

    Working on UI and connecting Auth for firebase

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import * as firebase from 'firebase';
-import firebaseConfig from './config/config';
+import firebase from 'firebase';
+import { firebaseConfig } from './config/config';
 
 import LoadingScreen from './screens/LoadingScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -14,4 +15,28 @@ import RegisterScreen from './screens/RegisterScreen';
 
 firebase.initializeApp(firebaseConfig);
 
-const AppStack = createStackNavigator();
+const AppStack = createStackNavigator({
+    Home: HomeScreen
+});
+
+const AuthStack = createStackNavigator({
+    Login: LoginScreen,
+    Register: RegisterScreen
+});
+
+export default createAppContainer(
+    createSwitchNavigator(
+        {
+            Loading: LoadingScreen,
+            App: AppStack,
+            Auth: AuthStack
+        },
+        {
+            initialRouteName: "Loading"
+        }
+    )
+)
+
+const styles = StyleSheet.create({
+
+})
--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import firebase from 'firebase';
 
 export default class LoadingScreen extends Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.props.navigation.navigate(user ? "App" : "Auth");
+        })
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>Loading Screen</Text>
+                <Text>Loading...</Text>
+                <ActivityIndicator size="Large"></ActivityIndicator>
             </View>
         )
     }

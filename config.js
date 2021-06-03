0b9b5556d353c0bb0e899cbcb5f99af769c9288c
Connor Clark <59457919+Voltaire01-W@users.noreply.github.com>
2021-06-03 11:00:15 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBVOCrDAcAI4yS2pkq9auC4AanU2SwyifY",
-    authDomain: "firebird-social.firebaseapp.com",
-    projectId: "firebird-social",
-    storageBucket: "firebird-social.appspot.com",
-    messagingSenderId: "316297248721",
-    appId: "1:316297248721:web:4f6569f26963737b53aaf0",
-    measurementId: "G-DB9591WWZW"
-  };

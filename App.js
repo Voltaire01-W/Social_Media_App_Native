7314d7176194165e7eea3be134d3cb3f593c1006
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-06-03 10:53:24 -0500

    Added Screens for register, login, loading, home, added new firebase config file.

--- App.js
+++ App.js
@@ -4,3 +4,9 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons } from "@expo/vector-icons";
+import * as firebase from 'firebase';
+
+import LoadingScreen from './screens/LoadingScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
